1b056cf22454d3ab294e270aea6ad0cf59419731
Matenia Rossides <matenia@gmail.com>
2016-02-19 11:26:59 +1100

    add health_check_helper

---
+++ lib/health_check/health_check_helper.rb
@@ -0,0 +1,6 @@
+# Some Rails2 settings require matching helper files for controllers.
+# This is the matching helper for `lib/health_check/health_check_controller.rb`
+module HealthCheck
+  module HealthCheckHelper
+  end
+end
